a4e66cbe0ba874c8db117b0d7e96a3e45f0b588d
herbertboy0801 <qaz2348@gmail.com>
2025-06-07 04:23:29 +0800

    Update tool-library-data.js via admin tool

--- admin/tool-library-data.js
+++ admin/tool-library-data.js
@@ -220,7 +220,7 @@ window.toolLibraryData = [
     "id": "tools-1749239489421",
     "name": "图片大小变更工具",
     "description": "可以自由调整图片的大小( by 比例或 by 像素)",
-    "icon": "palette",
+    "icon": "analytics",
     "category": "图像影音处理",
     "tags": [
       "图片调整"
