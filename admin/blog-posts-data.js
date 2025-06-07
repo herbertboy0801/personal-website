9f8042f8c2555e2293add3086387c72c0fba699d
herbertboy0801 <qaz2348@gmail.com>
2025-06-07 18:39:19 +0800

    Update blog-posts-data.js via admin tool

--- admin/blog-posts-data.js
+++ admin/blog-posts-data.js
@@ -5,34 +5,39 @@ window.blogPosts = [
     "source": "小红书",
     "title": "AI编程，3分钟创作属于自己的日记小程序",
     "summary": "晨间日记让你更清楚自己的方向，透过AI编程创造出好用工具来记录自己的每一天，加油",
-    "link": "https://www.xiaohongshu.com/explore/683dc394000000002202e6f2?xsec_token=AB1i-bXFYUGW7aGNw_tJTin9RpA85VQ7fYMWzbN5I4ik=&xsec_source=pc_user"
+    "link": "https://www.xiaohongshu.com/explore/683dc394000000002202e6f2?xsec_token=AB1i-bXFYUGW7aGNw_tJTin9RpA85VQ7fYMWzbN5I4ik=&xsec_source=pc_user",
+    "featured": true
   },
   {
     "id": "blog-1717682256002",
     "source": "小红书",
     "title": "AI编程，如何3分钟做出六一儿童节小游戏",
     "summary": "透过AI制作儿童节元素的射击游戏",
-    "link": "https://www.xiaohongshu.com/explore/683c6f12000000002300d6f5?xsec_token=ABdSk6o16B6NyzFjVKagFu1PgPtdxpAOSFoth2nUnQl6g=&xsec_source=pc_user"
+    "link": "https://www.xiaohongshu.com/explore/683c6f12000000002300d6f5?xsec_token=ABdSk6o16B6NyzFjVKagFu1PgPtdxpAOSFoth2nUnQl6g=&xsec_source=pc_user",
+    "featured": false
   },
   {
     "id": "blog-1717682256003",
     "source": "小红书",
     "title": "AI编程，如何3分钟搞出一个端午节的小游戏",
     "summary": "端午节元素的接物游戏，小朋友玩的不亦乐乎",
-    "link": "https://www.xiaohongshu.com/explore/683b12a1000000002202d8d5?xsec_token=ABqQffvb-Yfujb1IlT4het5LdvHgL1TXBGmWTX0oRHbqp=&xsec_source=pc_user"
+    "link": "https://www.xiaohongshu.com/explore/683b12a1000000002202d8d5?xsec_token=ABqQffvb-Yfujb1IlT4het5LdvHgL1TXBGmWTX0oRHbqp=&xsec_source=pc_user",
+    "featured": false
   },
   {
     "id": "blog-1717682256004",
     "source": "公众号",
     "title": "零代码！AI编程3分钟生成你的专属工具箱",
     "summary": "目标透过AU编程完成100个实用小工具集合",
-    "link": "https://mp.weixin.qq.com/s/_ejVZtqMMRKAfCDA-tRdqq?scene=1"
+    "link": "https://mp.weixin.qq.com/s/_ejVZtqMMRKAfCDA-tRdqq?scene=1",
+    "featured": false
   },
   {
     "id": "blog-1717682256005",
     "source": "公众号",
     "title": "0基础学 COZE 古诗词视频详细教程：小白保证学会，图生视频工作流大拆解",
     "summary": "透过工作流，5分钟生成古诗词讲解短视频( 动画+音乐版)",
-    "link": "https://mp.weixin.qq.com/s/-IoNQuKONqomWbvjuhYtQA"
+    "link": "https://mp.weixin.qq.com/s/-IoNQuKONqomWbvjuhYtQA",
+    "featured": false
   }
 ];
