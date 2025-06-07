a509bc9dbff1a5715dead548d63bbc6b49945661
herbertboy0801 <qaz2348@gmail.com>
2025-06-08 02:52:22 +0800

    Update journal-settings.js via admin tool

--- admin/journal-settings.js
+++ admin/journal-settings.js
@@ -1,8 +1,8 @@
 // journal-settings.js
 window.journalSettings = {
-  "referenceDate": "2025-06-07",
+  "referenceDate": "2025-06-08",
   "referenceStreak": 10,
   "goalDays": 100,
-  "goalReward": "手动检查测试",
+  "goalReward": "123",
   "reminderTime": "06:00"
 };
